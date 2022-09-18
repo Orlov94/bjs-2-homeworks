630f40cda1fb0850a591c3d0ad614ec33f631bb4
Vladislav <vladislav_orlov_94@mail.ru>
2022-09-18 20:39:13 +0300

    initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,27 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  const arr = [];
+  const discriminant = Math.pow(b, 2) - 4 * a * c; 
+  if (discriminant > 0) {   
+    let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(x1, x2);
+  }
+  else if (discriminant === 0) {   
+    let x1 = -b / (2 * a);
+    arr.push(x1);
+  }
+  else if (discriminant < 0) {  
+  }
+  
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
-  // код для задачи №2 писать здесь
+// function calculateTotalMortgage(percent, contribution, amount, date) {
+//   let totalAmount;
 
-  return totalAmount;
-}
+//   // код для задачи №2 писать здесь
+
+//   return totalAmount;
+// }
