ec4daa27205cad0eba2aa01ec67a09325c749544
Vladislav <vladislav_orlov_94@mail.ru>
2022-09-26 19:39:22 +0300

    initial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,50 @@
 // Задание 1
+"use strict";
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+  for(let i = 0; i < arr.lenght; i++){
+    if(arr[i] > max)
+    max = arr[i];
 
-  // Ваш код
+    if(arr[i] < min)
+    min = arr[i];
+
+    sum = arr[i] + sum;
+
+  }
+
+  avg = Number((sum / arr.lenght).toFixed(2))
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
+  let sum = 0;
+  for(let i = 0; i < arr.lenght; i++){
+    sum = sum + arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
+  for(let i = 0; i < arrOfArr.lenght; i++){
+    const calculatingElements = func(arrOfArr[i]);
+    if(calculatingElements > max){
+      max = calculatingElements;
+    }
+  }
 
-  // Ваш кода
-  // for ...
-  
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let res = getArrayParams(arr);
+  return Math.abs(res.max - res.min);
 }
