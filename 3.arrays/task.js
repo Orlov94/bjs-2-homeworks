0da4335d825c9b044922bc5acca2c03ecf02c699
Vladislav <vladislav_orlov_94@mail.ru>
2022-09-29 20:54:21 +0300

    update method

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  result = (arr1.length === arr2.length) && arr1.every((elment, index) => element === arr2[index])
+  result = (arr1.length === arr2.length) && arr1.every((elment, index) => elment === arr2[index])
 
   return result; // boolean
 }
@@ -9,7 +9,7 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  resultArr = arr.filter((number) => number > 0) filter((number) =>number % 3 === 0) map((number) => number * 10)
+  resultArr = arr.filter((number) => number > 0).filter((number) =>number % 3 === 0).map((number) => number * 10)
 
   return resultArr; // array
 }
