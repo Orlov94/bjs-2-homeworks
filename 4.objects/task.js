838db14ef9d164dcdbee23b020616ef87b30670e
Vladislav <vladislav_orlov_94@mail.ru>
2022-10-02 18:58:35 +0300

    creation objects for student

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,21 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name;
+    this.gender;
+    this.age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+let student = new Student ("Владимир", "мужской", 21 )
+let student1 = new Student ("Анна", "женский", 19 )
+let student2 = new Student ("Екатерина", "женсикй", 22 )
 
-// ваш код для остальных методов
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
+}
+Student.prototype.addMark = function(mark) {
+  if(this.marks === undefined){
+    this.marks = mark;
+  }else{
+    Student.prototype.addMarks = function (...mark){
+    }
+  }
+}
